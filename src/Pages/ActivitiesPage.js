2bf36a99bd4ad45b8100a6a07e239faadcb370ff
Hammad Jehangir <hammadj7@gmail.com>
2023-03-29 18:59:00 +0100

    Added functionality to Activities page
    Created firestore queries to find all activities for a user using their user ID and displays them in a form. Next step is to add a graph to visualise the data.

--- src/Pages/ActivitiesPage.js
+++ src/Pages/ActivitiesPage.js
@@ -1,69 +1,71 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { doc, getDoc, updateDoc} from "firebase/firestore"
+import { collection, query, where, getDocs } from "firebase/firestore";
 import { auth, db} from "../firebase-config";
 import background from "../wallpaper.jpeg";
 
 export default function ActivitiesPage() {
     const userID = auth.currentUser.uid;
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [weight, setWeight] = useState("");
-    const [height, setHeight] = useState("");
-    const [age, setAge] = useState("");
-    const [error, setError] = useState("");
+    const [activityList, setActivityList] = useState([])
+    const [activity, setActivity] = useState("");
+    const [calories, setCalories] = useState("");
+    const [dateTime, setDateTime] = useState("");
+    const [distance, setDistance] = useState("");
+    const [distanceUnit, setDistanceUnit] = useState("");
+    const [time, setTime] = useState("");
     
     useEffect(() => {
         getActivities();
     }, []);
 
     async function getActivities() {
-        const docRef = doc(db, "Users", userID);            
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
-          setFirstName(docSnap.data().firstName);
-          setLastName(docSnap.data().lastName);
-          setWeight(docSnap.data().weight);
-          setHeight(docSnap.data().height);
-          setAge(docSnap.data().age);
-        } else {
-          console.log("No such document!");
-        }
-    }
-
-    const validation = () => {
-        if(firstName === "" || lastName === ""){
-            setError("Please enter first/last name")
-        }
-        if(weight === "" || height === "" || age === ""){
-            setError("Please enter weight/height/age")
-        }
-        else{
-            setError("")
-            updateDetails();
-        }
-    }
-
-    const updateDetails = () => {
-        async function res() {
-        await updateDoc(doc(db, "Users", userID), {
-            firstName: firstName,
-            lastName: lastName,
-            weight: weight,
-            height: height,
-            age: age
-            });
-        console.log("User details updated")
-        }
-        res()
+        const q = query(collection(db, "Activities"), where("userID", "==", userID))
+        const querySnapshot = await getDocs(q);
+        const activities = [];
+        querySnapshot.forEach((doc) => {
+            console.log(doc.id, " => ", doc.data());
+            
+            activities.push(doc.data())
+        });
+        setActivityList(activities)
     }
 
     return(
         <div className='pageContainer'>
+
+        <div className='formContainer'>
             <div className='form'>
-                <h3 className='formTitle'> Activities </h3>
-                
+            <h3 className='formTitle'> Graph </h3>
+            
             </div>
+
+            <div className='form'>
+            <h3 className='formTitle'> Activities </h3>
+            <div>
+                {activityList.map((data, index) => {
+                return (
+                    <div key={index} className='activity'>
+                    <p>Activity: {data.Activity} Date: {data.DateTime.toDate().toDateString()} </p>
+                    <p>Time: {data.DateTime.toDate().toLocaleTimeString('en-US')} Calories Burned: {data.CaloriesBurnt}</p>
+                    </div>
+                );
+                })}
+            </div>
+            </div>
+        </div>
+
+        <div className='formContainer'>
+            <div className='form'>
+            <h3 className='formTitle'> Stats </h3>
+            
+            </div>
+
+            <div className='form'>
+            <h3 className='formTitle'> Pie Chart </h3>
+            
+            </div>
+        </div>
+
         </div>
 
     )
